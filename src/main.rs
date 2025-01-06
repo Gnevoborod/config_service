1804e76bf122484365f214eaad51a5cf4f3afdc1
p.dorokhov <p.dorokhov@datareon.ru>
2025-01-06 14:17:28 +0300

    прикручен запуск ноды

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use ipipe::Pipe;
-use std::{i32, io::{Read, Write}, path::PathBuf, thread, time::Duration};
+use std::{i32, io::{Read, Write}, path::PathBuf, thread, time::Duration, vec};
 mod common_utils;
 use common_utils::*;
 use std::env;
@@ -14,9 +14,6 @@ struct System
 
 fn main()
 {
-    // let mut to_config_pipe = Pipe::with_name("to_config_pipe").unwrap();
-    // let mut from_config_pipe = Pipe::with_name("from_config_pipe").unwrap();
-
     println!("config service started");
     let args:Vec<_> = env::args().collect();
     //виндовая реализация, для линухов, разумеется, будет иначе
@@ -30,13 +27,14 @@ fn main()
     else {
         path_to_platform.push("C:\\ProgramData\\Datareon\\Platform");
     }
+    let mut path_to_local_node_config = path_to_platform.clone();
+    path_to_local_node_config.push("LocalNodeConfig.json");
     path_to_platform.push("Cache");
     //тут грузим конфиг
     path_to_platform.push("DatareonPlatformNode");
     path_to_platform.push("ClusterCache.json");
 
 
-//let cluster_cache: serde_json::Value = load_json_from_file("C:\\ProgramData\\Datareon\\Platform\\Cache\\DatareonPlatformNode\\ClusterCache.json");
     let cluster_cache = load_json_from_file(path_to_platform.to_str().unwrap());
     let systems = cluster_cache.get("Systems").unwrap().as_array().unwrap();
     let mut guids:Vec<Uuid> = vec![];
@@ -49,29 +47,16 @@ fn main()
             guids.push(Uuid::parse_str(val).unwrap());
         }
     }
+    let local_node_config = load_json_from_file(path_to_local_node_config.to_str().unwrap());
+    let node_id = local_node_config.get("NodeId").unwrap().clone();
     
-    //закончили - отправляем обратно инфо, что всё ок
-    let t = thread::spawn(move ||answer_handler(&guids));
-    t.join();
-    // let mut next_command:[u8;4] =[0;4];
-    // loop {
-    //     if to_config_pipe.read(&mut next_command).unwrap() > 0 
-    //     {
-    //         if Commands::get_command(i32::from_le_bytes(next_command)) == Commands::GetConfig {//запрос конфига
-    //             println!("отправляем конфиг");
-    //             send_services_to_start(&mut from_config_pipe, &guids);
-    //         }
-    //     }
-    //     //thread::sleep(Duration::from_millis(50));
-    // }
 
-// for line in std::io::BufReader::new(pipe).lines()
-// {
-//     println!("{}",line.unwrap());
-// }
+    //закончили - отправляем обратно инфо, что всё ок
+    let t = thread::spawn(move ||answer_handler(&guids, &(Uuid::parse_str(node_id.as_str().unwrap()).unwrap())));
+    t.join();
 }
 
-fn answer_handler(guids: &Vec<Uuid>)
+fn answer_handler(guids: &Vec<Uuid>, node: &Uuid)
 {
     let mut to_config_pipe = Pipe::with_name("to_config_pipe").unwrap();
     let mut from_config_pipe: Pipe = Pipe::with_name("from_config_pipe").unwrap();
@@ -79,17 +64,25 @@ fn answer_handler(guids: &Vec<Uuid>)
     let mut next_command:[u8;4] =[0;4];
     loop {
         let mut buf = read_to_end(&mut to_config_pipe);
-        //if to_config_pipe.read(&mut next_command).unwrap() > 0 
         if buf.len()>0
         {
             next_command = [buf[0], buf[1], buf[2], buf[3]];
-            if Commands::get_command(i32::from_le_bytes(next_command)) == Commands::GetAdaptersConfig {//запрос конфига
-                println!("отправляем конфиг");
-                send_services_to_start(&mut from_config_pipe, &guids);
+            let mut command = i32::from_le_bytes(next_command);
+            match Commands::get_command(command)
+            {
+                Commands::GetAdaptersConfig => {println!("отправляем конфиг");
+                                            send_services_to_start(&mut from_config_pipe, &guids);},
+                Commands::GetNodeConfig =>send_node_to_start(&mut from_config_pipe, &node),
+                _=> do_nothing(),
             }
+            
         }
-        //thread::sleep(Duration::from_millis(50));
     }
+}
+
+fn do_nothing()
+{
+    println!("сидим, пердим");
 }
 
 fn send_load_done(pipe: &mut Pipe)
@@ -100,8 +93,7 @@ fn send_load_done(pipe: &mut Pipe)
     push_bytes_i32(&mut datagram, command);
     push_bytes_i64(&mut datagram, size);
     println!("Пишем обратку");
-    pipe.write(&datagram);
-    //thread::sleep(Duration::from_millis(2000));
+    pipe.write(&datagram).unwrap();
 }
 
  fn send_services_to_start(pipe: &mut Pipe, guids: &Vec<Uuid>)
@@ -112,7 +104,18 @@ fn send_load_done(pipe: &mut Pipe)
     push_bytes_i32(&mut datagram, command);
     push_bytes_usize(&mut datagram, size);
     push_guids(&mut datagram, guids);
-    pipe.write(&datagram);
+    pipe.write(&datagram).unwrap();
+ }
+
+ fn send_node_to_start(pipe: &mut Pipe, node: &Uuid)
+ {
+    let command:i32= Commands::NodeStart as i32;
+    let size = 16;
+    let mut datagram:Vec<u8>= vec![];
+    push_bytes_i32(&mut datagram, command);
+    push_bytes_usize(&mut datagram, size);
+    push_guid(&mut datagram, node);
+    pipe.write(&datagram).unwrap();
  }
 
  pub fn push_bytes_i32(vector: &mut Vec<u8>, val: i32)
@@ -173,6 +176,14 @@ fn push_guids(vector: &mut Vec<u8>, guids: &Vec<Uuid>)
     }
 }
 
+fn push_guid(vector: &mut Vec<u8>,guid: &Uuid)
+{
+    for byte in guid.to_bytes_le()
+        {
+        vector.push(byte);
+        }
+}
+
 
 fn read_to_end(pipe: &mut Pipe)->Vec<u8>
 {
@@ -192,7 +203,7 @@ fn read_to_end(pipe: &mut Pipe)->Vec<u8>
             break 'looping;
         }
     }
-
+    pipe.flush();
     result
 }
 
